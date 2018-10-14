04a250cdd2a40c513b8d1bd7252a79eb5077414c
André Storhaug <andr3.storhaug@gmail.com>
2018-10-15 00:15:53 +0200

    Update build information and add header

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,20 @@
+const webpack = require("webpack");
 const path = require("path");
+const pkg = require('./package.json');
+
+const timeStamp = new Date().toLocaleTimeString();
+const date = new Date();
+const banner = `
+${pkg.name} v${pkg.version}       ${date}
+by ${pkg.author.name}    ${pkg.author.email}
+${pkg.homepage}
+
+Copyright: 2018 NTNU
+License: ${pkg.license}
+
+Build: [hash]
+`;
+
 
 module.exports = {
     mode: 'production',
@@ -22,5 +38,8 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.BannerPlugin({banner: banner}),
+    ]
 };
